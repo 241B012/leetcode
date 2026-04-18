9b475700100f47325b298ec42d7c38cdb25164bc
241B012 <241B012@JUETGUNA.IN>
2026-04-18 21:34:05 +0530

    Time: 255 ms (62.07%) | Memory: 19.2 MB (75.25%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -0,0 +1,22 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        def expand(l, r):
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                l -= 1
+                r += 1
+            return s[l+1:r]
+
+        res = ""
+        
+        for i in range(len(s)):
+            
+            temp1 = expand(i, i)
+            
+            temp2 = expand(i, i+1)
+            
+            if len(temp1) > len(res):
+                res = temp1
+            if len(temp2) > len(res):
+                res = temp2
+        
+        return res
