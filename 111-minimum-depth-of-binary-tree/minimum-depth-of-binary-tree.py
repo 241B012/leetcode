0398f56111874e2372b8304b7382036275bcaec2
241B012 <241B012@JUETGUNA.IN>
2026-05-04 08:42:52 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (67.78%) - LeetSync

---
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+        q = deque([(root, 1)])
+        
+        while q:
+            node, depth = q.popleft()
+            
+            # First leaf node → minimum depth
+            if not node.left and not node.right:
+                return depth
+            
+            if node.left:
+                q.append((node.left, depth + 1))
+            if node.right:
+                q.append((node.right, depth + 1))
