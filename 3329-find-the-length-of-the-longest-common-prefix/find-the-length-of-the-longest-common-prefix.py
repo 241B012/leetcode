214d4bb240d7a27fa1bc731cdddc5807521f7eab
241B012 <241B012@JUETGUNA.IN>
2026-05-21 10:08:07 +0530

    Time: 309 ms (87.78%) | Memory: 21.5 MB (94.44%) - LeetSync

---
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def longestCommonPrefix(self, arr1, arr2):
+        """
+        :type arr1: List[int]
+        :type arr2: List[int]
+        :rtype: int
+        """
+        
+        prefixes = set()
+        
+        # Store all prefixes from arr1
+        for num in arr1:
+            while num > 0:
+                prefixes.add(num)
+                num //= 10
+        
+        ans = 0
+        
+        # Check prefixes from arr2
+        for num in arr2:
+            while num > 0:
+                if num in prefixes:
+                    ans = max(ans, len(str(num)))
+                    break
+                num //= 10
+        
+        return ans
